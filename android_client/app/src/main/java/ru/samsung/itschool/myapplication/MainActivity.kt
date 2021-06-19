5428d0b6a1addc82ff9104489e21b68ed120a2c0
supervisor <d.yacenko@gmail.com>
2021-06-19 16:34:08 +0300

    first commit

---
+++ android_client/app/src/main/java/ru/samsung/itschool/myapplication/MainActivity.kt
@@ -0,0 +1,31 @@
+package ru.samsung.itschool.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import java.net.URL
+import javax.net.ssl.HttpsURLConnection
+
+class MainActivity : AppCompatActivity() {
+    lateinit var et_url: EditText
+    lateinit var text: EditText
+    lateinit var send: Button
+    lateinit var result: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        et_url = findViewById(R.id.url)
+        text = findViewById(R.id.text_to_send)
+        send = findViewById(R.id.send)
+        result =findViewById(R.id.result)
+        send.setOnClickListener(View.OnClickListener {sendText()})
+    }
+
+    private fun sendText() {
+        TODO("допишите свой код здесь!")
+    }
+}
